25e01feaf8e4ab87f0ce5ea3893a456d5e3830cb
theeasylearn <theeasylearn@gmail.com>
2024-04-16 08:10:59 +0530

    first commit

---
+++ switch1.c
@@ -0,0 +1,51 @@
+/*
+    write a program to accept day of week from user. then print name of the day.
+ day = 1 display monday
+ day = 2 display tuesday
+ day = 3 display wednesday
+ day = 4 display thursday
+ day = 5 display friday
+ day = 6 display saturday
+ day = 7 display sunday
+
+ */
+#include <stdio.h>
+void main()
+{
+    int day;
+    printf("enter day");
+    scanf("%d", &day);
+    switch (day)
+    {
+        case 1:
+        printf("it is monday");
+        break;
+
+        case 2:
+        printf("it is tuesday");
+        break;
+
+        case 3:
+        printf("it is wednesday");
+        break;
+
+        case 4:
+        printf("it is thursday");
+        break;
+
+        case 5:
+        printf("it is friday");
+        break;
+
+        case 6:
+        printf("it is saturday");
+        break;
+
+        case 7:
+        printf("it is sunday");
+        break;
+
+        default:
+        printf("it is not valid input");
+    }
+}
---
+++ switch2.c
@@ -0,0 +1,28 @@
+/* write a program to find use given letter is vowel or not */
+#include <stdio.h>
+void main()
+{
+    char letter;
+    printf("Enter any one letter");
+    scanf("%c", &letter);
+
+    switch (letter)
+    {
+    case 'a':
+    case 'e':
+    case 'i':
+    case 'o':
+    case 'u':
+    case 'A':
+    case 'E':
+    case 'I':
+    case 'O':
+    case 'U':
+        printf("it is vowel");
+        break;
+
+    default:
+        printf("it is not vowel");
+        break;
+    }
+}

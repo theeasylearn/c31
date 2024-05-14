6b7880207cb022ecea7ca02f3d7bda017f13dd6a
theeasylearn <theeasylearn@gmail.com>
2024-05-14 09:31:11 +0530

    structure 3

--- structure-2.c
+++ structure-2.c
@@ -13,7 +13,7 @@ struct student
 void main()
 {
     struct student batch[SIZE]; // array of strcture
-    for(int i=0;i<`;i++)
+    for(int i=0;i<SIZE;i++)
     {
         fflush(stdin);
         printf("\n enter %d student detail \n enter name",i+1);
@@ -28,5 +28,13 @@ void main()
         fflush(stdin);
         printf("enter gender (male=m, female=f) ");
         scanf("%c", &batch[i].gender);
+    }
+
+    //display student data
+    printf("fullname\t\t\tage\t\tweight\t\tgender\n ");
+    printf("============================================================================");
+    for(int i=0;i<SIZE;i++)
+    {
+        printf("\n%s %s \t\t\t%d\t\t%.2f\t\t%c",batch[i].surname,batch[i].name,batch[i].age,batch[i].weight,batch[i].gender);
     }
 }
---
+++ structure-3.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+// create constant
+#define SIZE 2
+#define SUBJECT_COUNT 3
+// create structure
+struct marksheet
+{
+    char fullname[32];           // array within structure
+    int subjects[SUBJECT_COUNT]; // array within structure
+    int total;
+    float percentage;
+    char result;
+}; //
+void main()
+{
+    struct marksheet batch[SIZE]; // array of structure
+    int temp=0;
+    for (int i = 0; i < SIZE; i++)
+    {
+        fflush(stdin);
+        printf("enter %d student detail", i);
+        printf("\nenter name");
+        gets(batch[i].fullname);
+
+        for(int j=0;j<SUBJECT_COUNT;j++)
+        {
+            printf("\n enter %d subject marks",j+1);
+            scanf("%d",&batch[i].subjects[j]);
+            temp = temp + batch[i].subjects[j];
+        }
+        if(batch[i].subjects[0]>=33 && batch[i].subjects[1]>=33 && batch[i].subjects[2]>=33)
+        {
+            batch[i].result = 'p';
+        }
+        else 
+        {
+            batch[i].result = 'f';
+        }
+        batch[i].total = temp;
+        batch[i].percentage = batch[i].total / SUBJECT_COUNT;
+    }
+}
